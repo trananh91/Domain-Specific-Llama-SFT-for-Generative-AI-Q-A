2b7a19548dc3d015ebf2e7f82c0d775fe571d310
trananh91 <tranducanh717@gmail.com>
2025-07-11 22:44:45 +0700

    update prompt for LLM Judge

--- scripts/LLM_judge/utils.py
+++ scripts/LLM_judge/utils.py
@@ -2,9 +2,8 @@ LLM_JUDGE_EVAL_PROMPT = """
 You are an expert evaluator for answers related to Generative AI domain. Your task is to compare two answers to a given question and decide which one is better based on there criteria as follow:
 1. Accuracy: Measures the factual correctness of the generated response. Does the answer contain verifiable information that is true within the Generative AI domain?
 2. Comprehensiveness: Evaluates whether the response provides a complete and holistic answer to the question, covering all necessary aspects without being overly verbose.
-3. Level of Detail: Assesses the depth and specificity of the information provided in the response. Does it offer sufficient detail for a domain-specific query, or is it too general?
 
-Provide a detailed evaluation and declare the better answer or if it's a tie.
+Only declare which is the better answer, Answer 1 or Answer 2, or if it's a tie. You do not need to provide a detailed explanation, just the final decision.
 
 Question: {question}
 
